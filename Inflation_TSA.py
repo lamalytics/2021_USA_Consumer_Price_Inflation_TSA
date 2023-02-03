97b188dc18ea68bc725445161ea4edaf085452cf
lamalytics <95007715+lamalytics@users.noreply.github.com>
2023-02-02 20:21:22 -0800

    Update with ARIMA forecast

--- Inflation_TSA.py
+++ Inflation_TSA.py
@@ -22,7 +22,7 @@ dataset.rename(columns={'index': 'date', 0: 'Annual_Inflation%'}, inplace=True)
 # remove brackets from date col
 dataset["date"] = dataset["date"].str.replace(" \\[.*\\]", "")
 # change date col to index and datetime
-dataset["date"] = pd.to_datetime(dataset["date"], format="%Y-%m-%d").dt.strftime('%m-%d-%Y')
+dataset["date"] = pd.to_datetime(dataset["date"], format="%Y-%m-%d")
 dataset["Annual_Inflation%"] = dataset["Annual_Inflation%"].astype(float)
 
 # set index to new date col
@@ -31,8 +31,8 @@ dataset.set_index("date", inplace=True)
 # print(dataset.tail())
 
 # period of high inflation in the 1970s, inflation starting to increase with 4% in 2021
-sns.displot(dataset)
-sns.scatterplot(dataset)
+# sns.displot(dataset)
+# sns.lineplot(data=dataset, x=dataset.index, y="Annual_Inflation%")
 
 
 # plt.show()
@@ -52,10 +52,11 @@ ad_results = adfuller(train_set['Annual_Inflation%'])
 # print(ad_results)
 # (-2.1665169105174336, 0.21862147991454323, 2, 59, {'1%': -3.5463945337644063, '5%': -2.911939409384601, '10%': -2.5936515282964665}, 195.73539928447815)
 
-# 1 order of diff helps, but may need more
+# find optimal diff to make time series stationary
 train_diff = train_set.diff()
-sns.scatterplot(train_diff)
+sns.lineplot(data=train_diff, x=train_diff.index, y="Annual_Inflation%")
 # plt.show()
+# optimal is diff x2
 
 
 # create plots of acf and pacf
@@ -71,38 +72,59 @@ plt.show()
 
 # based on plots, may be looking at ARMA (3,3)
 # order_aic_bic=pd.DataFrame({'p':[], 'q':[], 'AIC':[], 'BIC':[]})
-for p in range(3):
-    # Loop over q values from 0-2
-    for q in range(3):
-        try:
-            # create and fit ARMA(p,q) model
-            model = ARIMA(train_set, order=(p,0,q))
-            results = model.fit()
+# for p in range(4):
+#     # Loop over q values from 0-2
+#     for q in range(4):
+#         try:
+#             # create and fit ARMA(p,q) model
+#             model = ARIMA(train_set, order=(p,1,q))
+#             results = model.fit()
             
-            # Print order and results
-            print(p, q, results.aic, results.bic)
+#             # Print order and results
+#             print(p, q, results.aic, results.bic)
             
-        except:
-            print(p, q, None, None)
-# ARMA (1,0,1) is best from this
-# 0 0 231.7562689102466 235.41355170322478
-# 0 1 186.919057887745 192.40498207721228
-# 0 2 173.37902755856703 180.69359314452342
-# 1 0 182.15735680986313 187.64328099933041
-# 1 1 167.7095375105422 175.02410309649858
-# 1 2 169.62369418225194 178.7669011646974
-# 2 0 175.93028090621235 183.24484649216873
-# 2 1 169.6521647170993 178.79537169954477
-# 2 2 171.52828217503057 182.50013055396514
-# order_df = pd.DataFrame(order_aic_bic, 
-#                         columns=['p','q', 'AIC', 'BIC'])
+#         except:
+#             print(p, q, None, None)
+# ARMA (2,1,0) is best from this
+# 0 0 177.85893120220547 179.6655936919758
+# 0 1 167.40747531928073 171.02080029882137
+# 0 2 166.9913747172528 172.41136218656376
+# 1 0 176.1059684891346 179.71929346867523
+# 1 1 168.65623823378127 174.07622570309223
+# 1 2 166.16006460685935 173.38671456594062
+# 2 0 164.03705373748522 169.4570412067962
+# 2 1 165.84276033639014 173.0694102954714
+# 2 2 166.729739203939 175.7630516527906
 
-# # Print order_df in order of increasing AIC
-# print(order_aic_bic.sort_values('AIC'))
+best_model = ARIMA(train_set, order=(2,1,0))
+results = best_model.fit()
 
-# # Print order_df in order of increasing BIC
-# print(order_aic_bic.sort_values('BIC'))
+# plots KDE, resids
+# results.plot_diagnostics()
+# plt.show()
+
+# summary results to compare coeffs, Prob(Q) < 0.05 (reject null), Prob(JB) < 0.05 (reject null)
+# print(results.summary())
+# fail to reject null
+
+# seasonal patterns
+# acf does not show any significant evidence for seasonality
 
 
+# forecast results
+train_forecast = results.get_forecast(steps=test_set.shape[0], dynamic=True).predicted_mean
+train_forecast = pd.Series(train_forecast,index=train_forecast.index)
+train_forecast = pd.DataFrame(train_forecast)
+train_forecast.columns=["Annual_Inflation%"]
 
+print(test_set)
+print(train_forecast)
+plt.plot(train_set.index, train_set, label='observed')
+plt.plot(train_forecast.index, train_forecast, color='r', label='forecast')
+plt.plot(test_set.index, test_set, color='g', label='actual')
+plt.xlabel("Date")
+plt.xticks(rotation=90)
+plt.ylabel("Annual_Inflation%")
+plt.show()
 
+# looks like there is a seasonality factor, will need to find optimal period
